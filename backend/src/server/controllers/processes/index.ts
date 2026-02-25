d15aa99dc659deaa540398c6e3358e1fe07beb99
Conters <luangonfernandes@gmail.com>
2026-02-25 17:28:09 -0300

    Add CRUD operations for areas and processes, including validation and controller setup

---
+++ backend/src/server/controllers/processes/index.ts
@@ -0,0 +1,13 @@
+import * as Create from "./Create";
+import * as GetAll from "./GetAll";
+import * as GetByID from "./GetByID";
+import * as UpdateByID from "./UpdateByID";
+import * as DeleteByID from "./DeleteByID";
+
+export const ProcessesController = {
+  ...Create,
+  ...GetAll,
+  ...GetByID,
+  ...UpdateByID,
+  ...DeleteByID,
+};
--- backend/src/server/routes/index.ts
+++ backend/src/server/routes/index.ts
@@ -8,25 +8,6 @@ router.get("/", (req, res) => {
   res.send("olá mundo!");
 });
 
-router.get("/cidades", ensureAuthenticated);
-router.get("/cidades/:id", ensureAuthenticated);
-
-router.post("/cidades", ensureAuthenticated);
-
-router.put("/cidades/:id", ensureAuthenticated);
-
-router.delete("/cidades/:id", ensureAuthenticated);
-
-router.get("/pessoas", ensureAuthenticated);
-
-router.get("/pessoas/:id", ensureAuthenticated);
-
-router.post("/pessoas", ensureAuthenticated);
-
-router.put("/pessoas/:id", ensureAuthenticated);
-
-router.delete("/pessoas/:id", ensureAuthenticated);
-
 // router.post("/entrar/");
 // router.post("/cadastrar");
 
